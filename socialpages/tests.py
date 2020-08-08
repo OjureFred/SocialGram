4f0ef818434b240467d9775d5398a6f377ba79ec
Fred Ojure <fredojure@hotmail.com>
2020-08-08 15:57:16 +0300

    Added Test Case for save Editor

--- socialpages/tests.py
+++ socialpages/tests.py
@@ -7,4 +7,11 @@ class EditorTestClass(TestCase):
 
     #Set up method
     def setUp(self):
-        
+        self.fred = Editor(first_name='Fredrick', last_name='Ojure', email='fredojure@hotmail.com')
+    
+    #Test save method
+    def test_save_method(self):
+        self.fred.save_editor()
+        editors = Editor.objects.all()
+        self.assertTrue(len(editors) > 0)
+

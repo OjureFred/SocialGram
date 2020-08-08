5af687cdd5eb69ed95567b3243c2b7709af8c425
Fred Ojure <fredojure@hotmail.com>
2020-08-08 15:50:10 +0300

    Added tags and Posts models. Defined Foreign keys and Many To Many fields

--- socialpages/tests.py
+++ socialpages/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
+from .models import Editor, Post, tags
 
 # Create your tests here.
+
+class EditorTestClass(TestCase):
+
+    #Set up method
+    def setUp(self):
+        

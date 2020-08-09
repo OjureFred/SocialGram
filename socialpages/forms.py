58817a11ac65956354f2d07c08526538802ad68b
Fred Ojure <fredojure@hotmail.com>
2020-08-09 19:07:27 +0300

    Created form to post new Posts

--- socialpages/forms.py
+++ socialpages/forms.py
@@ -3,4 +3,11 @@ from django import forms
 class PostForm(forms.Form):
     your_name = forms.CharField(label='First Name', max_length=30)
     email = forms.EmailField(label='Email')
-    
+
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude['editor', 'pub_date']
+        widgets = {
+            'tags': forms.CheckboxSelectMultiple(),
+        }

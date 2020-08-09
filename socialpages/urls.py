22b7f9f02d99c79e1d834153511aff1eb1e2ce39
Fred Ojure <fredojure@hotmail.com>
2020-08-09 11:29:05 +0300

    Updated urls for image upload

--- socialpages/urls.py
+++ socialpages/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import url
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -6,3 +8,5 @@ urlpatterns = [
     url(r'^$', views.main_page, name='main_page'),
     url(r'^search/', views.search_results, name='search_results')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)

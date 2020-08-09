acbf10d990f9d2426ad4bf865f468de1e3a2939b
Fred Ojure <fredojure@hotmail.com>
2020-08-09 19:02:22 +0300

    Installed tinymce and converted post field to HTMLField

--- socialpages/models.py
+++ socialpages/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 import datetime as dt
 
 # Create your models here.
@@ -12,7 +13,7 @@ class tags(models.Model):
 
 class Post(models.Model):
     title = models.CharField(max_length=60)
-    post = models.TextField()
+    post = HTMLField()
     editor = models.ForeignKey(User, on_delete = models.CASCADE)
     tags = models.ManyToManyField(tags)
     pub_date = models.DateTimeField(auto_now_add=True)

12b77f123a6531795c66eeb77175bc5e8a334dd0
Fred Ojure <fredojure@hotmail.com>
2020-08-09 21:53:00 +0300

    Styled the main page and base template

--- socialpages/admin.py
+++ socialpages/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Editor, Post, tags
+from .models import  Post, tags
 
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     filter_horizontal = ('tags',)
 
-admin.site.register(Editor)
+
 admin.site.register(Post, PostAdmin)
 admin.site.register(tags)
--- socialpages/forms.py
+++ socialpages/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Post
 
 class PostForm(forms.Form):
     your_name = forms.CharField(label='First Name', max_length=30)
@@ -7,7 +8,7 @@ class PostForm(forms.Form):
 class NewPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        exclude['editor', 'pub_date']
+        exclude = ['editor', 'pub_date']
         widgets = {
             'tags': forms.CheckboxSelectMultiple(),
         }

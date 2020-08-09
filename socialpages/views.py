8505f84693718342739349e9bcd0f8e73c6ab654
Fred Ojure <fredojure@hotmail.com>
2020-08-09 10:25:14 +0300

    Updated template to show post titles

--- socialpages/views.py
+++ socialpages/views.py
@@ -3,9 +3,11 @@ from django.http import HttpResponse
 import datetime as dt
 
 # Other imports
+from .models import Post
 
 # Create your views here.
 def main_page(request):
     date = dt.date.today()
-    context = {'date': date,}
+    all_posts = Post.objects.all()
+    context = {'date': date, 'all_posts': all_posts}
     return render(request, 'main_page.html', context)
